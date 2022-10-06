85dd1dfb4a986516460d7ee14dbce5494f1ce652
Joko Setiawan <joko.setiawan@jasuindo.com>
2022-10-06 15:11:35 +0700

    up

---
+++ src/components/PdfViewer.vue
@@ -0,0 +1,18 @@
+
+<script>
+import PdfEditor from './PdfEditor.vue';
+export default {
+    props: {
+        src: {
+            required: true
+        }
+    },
+    components: {
+        PdfEditor
+    }
+}
+
+</script>
+<template>
+    <PdfEditor :src="src"/>
+</template>
